2caa88598c8b31e6f1e6bbd781f5379bf6707850
George Perdrizet <george@perdrizet.org>
2025-06-19 09:56:07 -0400

    Added timezone look-up tool.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from smolagents import CodeAgent,DuckDuckGoSearchTool, HfApiModel,load_tool,tool
+from smolagents import CodeAgent, DuckDuckGoSearchTool, HfApiModel, load_tool, tool
 import datetime
 import requests
 import pytz
@@ -55,7 +55,7 @@ with open("prompts.yaml", 'r') as stream:
     
 demo = CodeAgent(
     model=model,
-    tools=[final_answer], ## add your tools here (don't remove final answer)
+    tools=[get_current_time_in_timezone, final_answer], ## add your tools here (don't remove final answer)
     max_steps=6,
     verbosity_level=1,
     grammar=None,
